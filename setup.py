3819f41499b833f05ee4650cb92737de693ae731
Iain <iain.j.cruickshank@gmail.com>
2019-03-12 10:40:03 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gowers_method",
+    version="0.0.1",
+    author="Iain J. Cruickshank",
+    author_email="icruicks@andrew.cmu.edu",
+    description="Gower's Method for creating Latent Graphs from Multi-Modal Data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ijcruic/Gowers-Method",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
