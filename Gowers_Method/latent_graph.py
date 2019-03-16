62f1582c506d5d82a6064fd84e881ec57d5ebb8c
Iain <iain.j.cruickshank@gmail.com>
2019-03-16 12:32:09 -0400

    Update latent_graph.py

--- Gowers_Method/latent_graph.py
+++ Gowers_Method/latent_graph.py
@@ -8,6 +8,7 @@ acknowledgements: Work supported by National Science Foundation Graduate Researc
 """
 
 import pandas as pd, networkx as nx, community
+from Gowers_Method import data_operations, graph_computation
 
 class latent_graph:
     """Finds a latent graph of data using Gower's method and specified graph constructor
